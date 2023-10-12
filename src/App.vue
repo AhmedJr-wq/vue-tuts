1faa00b9ea631d7bf897f3945875612942ec283c
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-10-12 16:47:21 +0000

    created friend contact component

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <section>
+    <h2>Mayor of Kingstown</h2>
+    <ul>
+      <friendContact></friendContact>
+    </ul>
+  </section>
+</template>
+
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '33455666362',
+          email: 'mvfhgjgtj@l.com'
+        },
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '33455666362',
+          email: 'mvfhgjgtj@l.com'
+        },
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '33455666362',
+          email: 'mvfhgjgtj@l.com'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Jost ,serif;
+  //-webkit-font-smoothing: antialiased;
+  //-moz-osx-font-smoothing: grayscale;
+  //text-align: center;
+  //color: #2c3e50;
+  //margin-top: 60px;
+}
+</style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">Show details</button>
+    </li>
+    <ul v-if="showDetails">
+        <li>Phone: <strong>{{ friend.phone }}</strong></li>
+        <li>Email: <strong>{{ friend.email }}</strong></li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                showDetails: false,
+                friend: {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '3345 56663 6277',
+                    email: 'mvfhgjgtj@l.com'
+                }
+            }
+        },
+        methods: {
+            toggleDetails() {
+                this.showDetails = !this.showDetails
+            }
+        }
+    }
+</script>
